4ce41800fc07d6b5a4558b4b6d6ee38b36442e13
Svilen <svilen.ivanov01@gmail.com>
2022-11-16 01:01:57 +0200

    Fixed an InvalidOperationException: No suitable constructor was found for entity type 'Endpoint'

---
+++ MyWebPortfolio/MyWebPortfolio/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using MyWebPortfolio.Data;
+
+using Microsoft.AspNetCore.Mvc;
+using MyWebPortfolio.Models;
+
+namespace MyWebPortfolio.Controllers {
+    public class CategoryController : Controller {
+
+        private readonly AppdDbContext _db;
+
+        public CategoryController(AppdDbContext db) {
+            _db = db;
+        }
+
+        public IActionResult Index() {
+            IEnumerable<Category> categories = _db.Categories;
+            return View(categories);
+        }
+    }
+}
