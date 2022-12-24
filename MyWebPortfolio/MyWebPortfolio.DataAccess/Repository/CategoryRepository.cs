803d8520e97ac0f1a777e292305a36ee90ad6d7d
Svilen <svilen.ivanov01@gmail.com>
2022-12-24 22:22:18 +0200

    Created new IUnitOfWork interface and a class UnitOfWork that implements it.

--- MyWebPortfolio/MyWebPortfolio.DataAccess/Repository/CategoryRepository.cs
+++ MyWebPortfolio/MyWebPortfolio.DataAccess/Repository/CategoryRepository.cs
@@ -15,9 +15,6 @@ namespace MyWebPortfolio.DataAccess.Repository {
             _db = db;
 
         }
-        public void Save() {
-            _db.SaveChanges();
-        }
 
         public void Update(Category category) {
             
--- MyWebPortfolio/MyWebPortfolio.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ MyWebPortfolio/MyWebPortfolio.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -1,13 +1,12 @@
-﻿using MyWebPortfolio.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyWebPortfolio.DataAccess.Repository.IRepository {
-    public interface ICategoryRepository : IRepository<Category>{
-
-        public void Update(Category category);
+    public interface IUnitOfWork {
+        ICategoryRepository Category { get; }
+        public void Save();
     }
 }
