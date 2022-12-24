7069eb768cb06f4890d9a811b32ef226d13b4138
Svilen <svilen.ivanov01@gmail.com>
2022-12-24 21:17:05 +0200

    Added ICategoryRepository and CategoryRepository that implements Repository and ICategoryRepository

---
+++ MyWebPortfolio/MyWebPortfolio.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using MyWebPortfolio.DataAccess.Data;
+using MyWebPortfolio.DataAccess.Repository.IRepository;
+using MyWebPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebPortfolio.DataAccess.Repository {
+    public class CategoryRepository : Repository<Category>, ICategoryRepository {
+
+        private readonly AppdDbContext _db;
+        public CategoryRepository(AppdDbContext db) : base(db) {
+            _db = db;
+            
+        }
+        public void Save() {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category category) {
+            _db.Update(category);
+        }
+    }
+}
---
+++ MyWebPortfolio/MyWebPortfolio.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using MyWebPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebPortfolio.DataAccess.Repository.IRepository {
+    public interface ICategoryRepository : IRepository<Category>{
+
+        public void Update(Category category);
+        public void Save();
+    }
+}
