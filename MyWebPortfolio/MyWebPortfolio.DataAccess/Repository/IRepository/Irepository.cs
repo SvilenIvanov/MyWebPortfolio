2f99931bf4f0458cd5fad64cde20103ac96c5966
Svilen <svilen.ivanov01@gmail.com>
2022-12-24 19:42:08 +0200

    Implementing repository pattern.
    Added a IRepository interface

---
+++ MyWebPortfolio/MyWebPortfolio.DataAccess/Repository/IRepository/Irepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebPortfolio.DataAccess.Repository.IRepository {
+    public interface Irepository<T> where T : class {
+
+        public void Add(T entity);
+        public void Remove(T entity);
+        public void RemoveRange(IEnumerable<T> entities);
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter);
+        public T GetSingleOrDefault(Expression<Func<T, bool>> filter);
+        public IEnumerable<T> GetAll();
+    }
+}
