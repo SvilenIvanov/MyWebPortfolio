faa12dd5a571585d317f70cf1d9e1f624becf41d
Svilen <svilen.ivanov01@gmail.com>
2022-12-28 01:44:18 +0200

    Added a validation for creating a product with empty image. Created a sweetalert if a product with no image is trying to be created.

--- MyWebPortfolio/MyWebPortfolio.Models/ViewModels/ProductVM.cs
+++ MyWebPortfolio/MyWebPortfolio.Models/ViewModels/ProductVM.cs
@@ -9,8 +9,8 @@ namespace MyWebPortfolio.Models.ViewModels {
     public class ProductVM {
 
         public Product product;
-        public IEnumerable<SelectListItem> CategoryList {get; set;}
-        public IEnumerable<SelectListItem> CoverList {get; set;}
+        public IEnumerable<SelectListItem>? CategoryList {get; set;}
+        public IEnumerable<SelectListItem>? CoverList {get; set;}
 
         public ProductVM() {
             product = new Product();
